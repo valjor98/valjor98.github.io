11aa30323a811f6749446fbb9ae635d10ff4b194
valjor98 <valjor98@gmail.com>
2023-07-21 17:07:10 -0600

    development of nav bar

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,14 @@
+import {
+  HiHome,
+  HiUser,
+  HiViewColumans,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from 'react-icons/hi2';
+
 //  links
-const links = [
+export const navData = [
   { name: 'home', path: '/', icon: <HiHome /> },
   { name: 'about', path: '/about', icon: <HiUser /> },
   { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
@@ -16,8 +25,37 @@ const links = [
   },
 ];
 
+import Link from 'next/link';
+
+import { useRouter } from 'next/router'
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const path = router.pathname;
+  return 
+  <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max 
+  bottom-0 mt-auto xl:right-[2%] x-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+    {}
+    <div className='flex w-full xl:flex-col items-center justify-between 
+    xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 bg-white/10
+    backdrop-blur-sm text-3xl xl:text-xl xl_rounded-full'
+    >
+      {navData.map((link, index) => {
+        return (
+        <Link 
+        className={`${
+          link.path === pathname && 'text-accent'
+      } relative flex items-center group hover:text-accent transition-all
+      duration-300`} 
+      href={link.path} 
+      key= {index}
+      >
+          <div>{link.icon}</div>
+        </Link>
+        );
+      })}
+    </div>
+  </nav>;
 };
 
 export default Nav;
