e97331f3f760492b540d6a9122eae9456b09a8be
valjor98 <valjor98@gmail.com>
2023-07-25 14:15:10 -0600

    added main screen text

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,43 @@ import { fadeIn } from '../variants'
 const Home = () => {
   return (
     <div className='bg-primary/60 h-full'>
-      <div>
-        <div>
-          <h1 className='h1'>
+      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30
+      to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left
+        h-full container mx-auto'>
+          <motion.h1
+            variants={fadeIn('down', 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h1'
+          >
             Transforming Ideas <br /> Into{' '}
             <span className='text-accent'>Digital Reality</span>
-          </h1>
-          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
-            Lorem ipsum dolor sit amet consecteur adipisicing elit. Itaque 
-            Lorem ipsum dolor sit amet consecteur adipisicing elit. Itaque 
-            Lorem ipsum dolor sit amet consecteur adipisicing elit. Itaque 
-          </p> 
+          </motion.h1>
+          <motion.p 
+            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
+            variants={fadeIn('down', 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+          >
+              Lorem ipsum dolor sit amet consecteur adipisicing elit. Itaque 
+              Lorem ipsum dolor sit amet consecteur adipisicing elit. Itaque 
+              Lorem ipsum dolor sit amet consecteur adipisicing elit. Itaque 
+          </motion.p> 
           <div className='flex justify-center xl:hidden'>
             <ProjectBtm />
           </div>
+          <motion.div 
+            variants={fadeIn('down', 0.4)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='hidden xl:flex'
+          >
+            <ProjectBtm/>
+          </motion.div>
         </div>
       </div>
       <div>Image</div> 
