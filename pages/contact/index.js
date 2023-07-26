fc5064b6bdf92ca088bc0fc2703ba9921c546e33
valjor98 <valjor98@gmail.com>
2023-07-26 15:36:41 -0600

    bug fixes

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,23 @@
+import Circles from "../../components/Circles";
+
+import { BsArrowBarRight } from "react-icons/bs";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../../variants";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="h-full bg-primary/30">
+      <div className="container mx-auto py-32 text-center xl:text-left flex
+      items-center justify-center h-full">
+        {/* text */}
+        <h2>
+          Let's <span className="text-accent">connect.</span>
+        </h2>
+      </div>
+    </div>
+  )
 };
 
 export default Contact;
