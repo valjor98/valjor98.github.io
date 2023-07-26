ef445c28f422953310d724f7a239b559a8069d00
valjor98 <valjor98@gmail.com>
2023-07-26 03:36:49 -0600

    added text and slider to services page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import Image from 'next/image';
 
-
-
-
-
 import ParticlesContainer from '../components/ParticlesContainer';
 import ProjectsBtn from '../components/ProjectsBtn';
 import Avatar from '../components/Avatar';
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,70 @@
+import { RxCrop, RxPencil2, RxDesktop, RxReader, RxRocket } from 'react-icons/rx';
+
+
+export const serviceData = [
+  {
+    icon: <RxCrop />,
+    title: 'Branding',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon: <RxPencil2 />,
+    title: 'Design',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon: <RxDesktop />,
+    title: 'Development',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon: <RxReader />,
+    title: 'Copywriting',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon: <RxRocket />,
+    title: 'SEO',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+];
+
+import ServiceSlider from '../../components/ServiceSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+
+import {motion} from 'framer-motion'
+import {fadeIn} from '../../variants'
+
+//1.51:16
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+  <div className='h-full bg-primary/30 py-36 flex items-center'>
+    <Circles />
+    <div className='container mx-auto'>
+      <div className='flex flex-col xl:flex-row gap-x-8'>
+        {/* text */}
+        <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4
+        xl:mb-0'>
+          <h2 className='h2 xl:mt-8'>
+            My services <span className='text-accent'>.</span> 
+          </h2>
+          <p className='mb-4 max-w-[400px] mx-auto lg-mx-0'>
+            Lorem ipsum dolor sit, amet consectetur adipiscing elit. A eveniet
+            dignissimos. Lorem ipsum dolor sit, amet consectetur adipiscing elit. A eveniet
+            dignissimos.
+          </p>
+        </div>
+        <div className='w-full xl:max-w-[65%]'>
+          {/* slider */}
+          <ServiceSlider />
+        </div>
+      </div>
+    </div>
+    <Bulb/>
+  </div>
+  );
 };
 
 export default Services;
